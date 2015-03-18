d41d59c2b76ce975d3cc2b0493e9dc3c1346466c
rroggerr <rroggerr@users.noreply.github.com>
2015-03-18 14:15:01 -0400

    execv update

--- kern/arch/mips/syscall/syscall.c
+++ kern/arch/mips/syscall/syscall.c
@@ -83,70 +83,74 @@ syscall(struct trapframe *tf)
 	int callno;
 	int32_t retval;
 	int err;
-
+    
 	KASSERT(curthread != NULL);
 	KASSERT(curthread->t_curspl == 0);
 	KASSERT(curthread->t_iplhigh_count == 0);
-
+    
 	callno = tf->tf_v0;
-
+    
 	/*
 	 * Initialize retval to 0. Many of the system calls don't
 	 * really return a value, just 0 for success and -1 on
 	 * error. Since retval is the value returned on success,
 	 * initialize it to 0 by default; thus it's not necessary to
-	 * deal with it except for calls that return other values, 
+	 * deal with it except for calls that return other values,
 	 * like write.
 	 */
-
+    
 	retval = 0;
-
+    
 	switch (callno) {
 	    case SYS_reboot:
-		err = sys_reboot(tf->tf_a0);
-		break;
-
+            err = sys_reboot(tf->tf_a0);
+            break;
+            
 	    case SYS___time:
-		err = sys___time((userptr_t)tf->tf_a0,
-				 (userptr_t)tf->tf_a1);
-		break;
+            err = sys___time((userptr_t)tf->tf_a0,
+                             (userptr_t)tf->tf_a1);
+            break;
 #ifdef UW
-	case SYS_write:
-	  err = sys_write((int)tf->tf_a0,
-			  (userptr_t)tf->tf_a1,
-			  (int)tf->tf_a2,
-			  (int *)(&retval));
-	  break;
-	case SYS__exit:
-	  sys__exit((int)tf->tf_a0);
-	  /* sys__exit does not return, execution should not get here */
-	  panic("unexpected return from sys__exit");
-	  break;
-	case SYS_getpid:
-	  err = sys_getpid((pid_t *)&retval);
-	  break;
-	case SYS_waitpid:
-	  err = sys_waitpid((pid_t)tf->tf_a0,
-			    (userptr_t)tf->tf_a1,
-			    (int)tf->tf_a2,
-			    (pid_t *)&retval);
-	  break;
-    #if OPT_A2
+        case SYS_write:
+            err = sys_write((int)tf->tf_a0,
+                            (userptr_t)tf->tf_a1,
+                            (int)tf->tf_a2,
+                            (int *)(&retval));
+            break;
+        case SYS__exit:
+            sys__exit((int)tf->tf_a0);
+            /* sys__exit does not return, execution should not get here */
+            panic("unexpected return from sys__exit");
+            break;
+        case SYS_getpid:
+            err = sys_getpid((pid_t *)&retval);
+            break;
+        case SYS_waitpid:
+            err = sys_waitpid((pid_t)tf->tf_a0,
+                              (userptr_t)tf->tf_a1,
+                              (int)tf->tf_a2,
+                              (pid_t *)&retval);
+            break;
+#if OPT_A2
         case SYS_fork:
             err = sys_fork(tf,(pid_t *)(&retval));
             break;
-    #endif //OPT_A2
+            
+        case SYS_execv:
+            err = sys_execv((const char*)tf->tf_a0, (char **)tf->tf_a1);
+            break;
+#endif //OPT_A2
 #endif // UW
-
-	    /* Add stuff here */
- 
-	default:
-	  kprintf("Unknown syscall %d\n", callno);
-	  err = ENOSYS;
-	  break;
+            
+            /* Add stuff here */
+            
+        default:
+            kprintf("Unknown syscall %d\n", callno);
+            err = ENOSYS;
+            break;
 	}
-
-
+    
+    
 	if (err) {
 		/*
 		 * Return the error code. This gets converted at
@@ -168,7 +172,7 @@ syscall(struct trapframe *tf)
 	 */
 	
 	tf->tf_epc += 4;
-
+    
 	/* Make sure the syscall code didn't forget to lower spl */
 	KASSERT(curthread->t_curspl == 0);
 	/* ...or leak any spinlocks */
@@ -193,4 +197,3 @@ enter_forked_process(void *tf, unsigned long dummy)
     kfree(tf);
     mips_usermode(&stack_tf);
 }
-
